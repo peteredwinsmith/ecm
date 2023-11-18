28fe8704d0feaf84f0bc3ea33c1b9f2744d30c93
peteredwinsmith <psmith@rollcallsolutions.com>
2023-11-18 11:40:11 +0800

    Add files via upload

---
+++ toggleNavigationMenu2.js
@@ -0,0 +1,15 @@
+export default function initApp() {
+  const hamburgerBtn = document.getElementById('hamburger-button')
+  const mobileMenu = document.getElementById('mobile-menu')
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle('hidden')
+    mobileMenu.classList.toggle('flex')
+    hamburgerBtn.classList.toggle('toggle-btn')
+  }
+
+  hamburgerBtn.addEventListener('click', toggleMenu)
+  mobileMenu.addEventListener('click', toggleMenu)
+};
+
+console.log("toggleNavigationMenu ok");
